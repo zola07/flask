6ff571d397c67fa1a10cd89e8fcb89615161a655
zola07 <lazarmilivojevic07@gmail.com>
2019-02-01 11:25:53 +0100

    edit home and add link page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/link')
+def link():
+    return render_template('link.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
